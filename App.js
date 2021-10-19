1fc1846faa680e6c14497173f9d2e2842d3e4d4e
Liam-gosnell <liamgoz99@gmail.com>
2021-10-19 16:32:39 +0100

    environment setup, added store.js & navSlice

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from "react-redux"
+
+// 1) set up redux
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <Provider store={}>
+      <View style={styles.container}>
+      <Text>Lets Build UBER</Text>
       <StatusBar style="auto" />
     </View>
+    </Provider>
   );
 }
 
